2d7b13b1e4d779f7cc2144f164ccc30ab06f93bc
githubkausername <githubkiemail>
2024-04-22 00:37:04 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,12 @@
+import inquirer from "inquirer";
+//Declare a constant answer and assign it to the result of inquirer.prompt function 
+const answers = await inquirer.prompt([
+    {
+        name: "Sentence",
+        type: "input",
+        message: "Enter your  sentence to count the word"
+    }
+]);
+const words = answers.Sentence.trim().split(" ");
+// print the array of words to console 
+console.log(`Your sentence word count is ${words.length}`);
